3c06458820391bec679f14a1454a9c40c1630842
hbzhu <zhuhaibo1@126.com>
2019-01-31 16:16:25 +0800

    无法打开服务

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
+from .models import Post
 from django.shortcuts import render
 
-# Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    for count, post in enumerate(posts):
+        post_list.append("No.{}:".format(str(count)) + str(post)+"<br>")
+    return HttpResponse(post_lists)
